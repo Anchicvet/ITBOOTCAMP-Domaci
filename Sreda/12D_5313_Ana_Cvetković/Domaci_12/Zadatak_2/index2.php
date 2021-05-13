1b329f2832edf6b54e59a4ab691ab87528c1c403
Anchicvet <83837426+Anchicvet@users.noreply.github.com>
2021-05-13 14:59:33 +0200

    izmenjen index2.php

--- Sreda/12D_5313_Ana_Cvetković/Domaci_12/Zadatak_2/index2.php
+++ Sreda/12D_5313_Ana_Cvetković/Domaci_12/Zadatak_2/index2.php
@@ -8,14 +8,14 @@
 </head>
 <body>
     <?php
-    $red = red;
-    $blue = blue;
+    $red = "red";
+    $blue = "blue";
 
     echo "<span style='color:red'>$red</span>";
     echo "<br>";
     echo "<span style='color:blue'>$blue</span>";
     echo "<br>";
-    echo "<a href=http://localhost/domaci_zadaci/Nedelja4/Sreda/Domaci_12/Zadatak_3/index3.php>Link ka zadatku 3</a>"
+    echo "<a href=http:'../zadatak_3/index3.php/'>Link ka zadatku 3</a>"
     ?>
 </body>
 </html>
