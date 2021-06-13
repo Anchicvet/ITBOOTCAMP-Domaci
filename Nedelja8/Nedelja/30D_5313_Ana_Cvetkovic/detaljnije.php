dd628f31be195dd6bed962547f99e730da526ab8
Ana <cvetkovicana888@gmail.com>
2021-06-13 04:20:11 +0200

    domaci 13.06

---
+++ Nedelja8/Nedelja/30D_5313_Ana_Cvetkovic/detaljnije.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="font-size:2em">
+    <?php
+
+        $h->header();
+        $m->meni();
+
+        if(!isset($_GET['id'])){
+            die("Trazite nepostojecu stranicu!");
+        }
+        $id =  $_GET['id'];
+        include "index1.php";
+        $lp->prikazi_jedan_proizvod_detaljnije($id);
+
+        $f->footer();
+    ?>
+</body>
+</html>
